bf441d99f97bc1d257f738883c21164ffad37500
AK18k <avi18000@gmail.com>
2023-10-15 22:23:40 +0300

    test llm on python code.

--- evals/lambada.py
+++ evals/lambada.py
@@ -1,11 +1,16 @@
 import torch 
-
 import argparse
 import os
 import sys
 import yaml 
 from tqdm import tqdm
 import json 
+from collections import Counter
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+
 
 sys.path.append(os.environ.get("SAFARI_PATH", "."))
 
@@ -29,10 +34,17 @@ def preprocess(text):
 
 class LAMBADA:
     "LAMBADA (OpenAI) benchmark"
-    def __init__(self, data_dir=None, use_stop_filter:bool=False):
-        data_dir = os.environ.get("DATA_DIR", data_dir)
-        lambada_path = os.path.join(data_dir + "/lambada/lambada_openai/lambada_test.jsonl")
-        self.data = [preprocess(json.loads(line)['text']) for line in open(lambada_path)] 
+    def __init__(self, data_dir=None, use_stop_filter:bool=False, use_code_data:bool=True):
+        self.use_code_data_ = use_code_data
+        if self.use_code_data_:
+            with open('/home/avi/data/code/python/final/jsonl/test/python_test_0.jsonl', 'r') as f:
+                sample_file = f.readlines()
+            self.data = sample_file
+        else:
+            data_dir = os.environ.get("DATA_DIR", data_dir)
+            lambada_path = os.path.join(data_dir + "/lambada/lambada_openai/lambada_test.jsonl")
+            self.data = [preprocess(json.loads(line)['text']) for line in open(lambada_path)] 
+        
         self.use_stop_filter = use_stop_filter 
     
     def run(self, model_cfg, ckpt_path):
@@ -54,15 +66,38 @@ class LAMBADA:
                     stop_filter[token_to_idx[' '+word]] = -float('inf')
             
         results = []
-        for prompt in tqdm(self.data):
-            target = prompt.split(" ")[-1]
-            
-            if isinstance(tokenizer, Tokenizer):
-                tokenized_prompt = tokenizer.encode(prompt).ids
-                target_tokenized = tokenizer.encode(' '+target).ids
+        tokenized_target = []
+        skiped_prompts_count = 0
+        target_token_index = -20
+        process_data_samples_limit = 1_000
+        for prompt in tqdm(self.data[:process_data_samples_limit]):
+            if (self.use_code_data_):
+                function_code_and_comments = json.loads(prompt)
+
+                code_tokens= function_code_and_comments['code_tokens']
+
+                # create string seperated by spaces from code tokens
+                code_string = ' '.join(code_tokens)
+
+                tokenized_prompt = tokenizer.encode(code_string)
+                target_token_index_actual = max(target_token_index, -len(tokenized_prompt))
+                target_tokenized = [tokenized_prompt[target_token_index_actual]]
             else:
-                tokenized_prompt = tokenizer.encode(prompt)
-                target_tokenized = tokenizer(' '+target)['input_ids']
+                target = prompt.split(" ")[-1]
+                
+                if isinstance(tokenizer, Tokenizer):
+                    tokenized_prompt = tokenizer.encode(prompt).ids
+                    target_tokenized = tokenizer.encode(' '+target).ids
+                else:
+                    tokenized_prompt = tokenizer.encode(prompt)
+                    target_tokenized = tokenizer(' '+target)['input_ids']
+
+            tokenized_target.append(target_tokenized)
+
+            if len(tokenized_prompt) > 1024:
+                skiped_prompts_count += 1
+                continue
+
             out = model(torch.tensor([tokenized_prompt]).to(device=device))
             
             if type(out) == tuple: out = out[0]
@@ -71,12 +106,52 @@ class LAMBADA:
             logits = logits + stop_filter[None]
             preds = logits.argmax(-1)            
             acc = all([pred == answer for pred, answer 
-                       in zip(preds[-len(target_tokenized):], target_tokenized)
-                    ]
-                )
+                       #in zip(preds[-len(target_tokenized):], target_tokenized)
+                       in zip(preds[target_token_index_actual:], target_tokenized)
+                    ])          
+
+            # # print target and predicted tokens
+            # target_token = tokenizer.decode(target_tokenized)
+            # predicted_token = tokenizer.decode(preds[-len(target_tokenized):])
+            # print(f"target token = {target_token}, predicted token = {predicted_token}")
+
+            # #if target token is not ")" then print the 10 last tokens of the prompt
+            # if target_token != predicted_token:
+            #     print(f"prompt = {tokenizer.decode(tokenized_prompt[-10:])}")
+
+            # # print accuracy
+            # print(f"Accuracy {acc}")
+
             results.append(acc)
             
         print(f"Accuracy {torch.tensor(results).float().mean().item()*100:4.2f}")
+
+        print(f"Skipped prompts {skiped_prompts_count}")
+
+        # create a list of target tokens
+        target_tokens = [tokenizer.decode(tokenized_target[i]) for i in range(len(tokenized_target))]
+
+
+        # count the frequency of each target token
+        token_counts = Counter(target_tokens)
+
+        # calculate the percentage of each token
+        total_tokens = sum(token_counts.values())
+        token_percentages = {token: count/total_tokens*100 for token, count in token_counts.items()}
+
+        # sort the tokens by their percentage values
+        sorted_tokens = sorted(token_percentages.items(), key=lambda x: x[1], reverse=True)
+
+        # plot a histogram of the token percentages
+        fig, ax = plt.subplots()
+        ax.bar([token[0] for token in sorted_tokens], [token[1] for token in sorted_tokens])
+        ax.set_ylabel('Percentage')
+        ax.set_xlabel('Token')
+        ax.set_ylim([0, 100])
+        ax.set_title('Token percentages')
+        plt.show(block=True)        
+
+
         
             
     def load_model(self, model_cfg, ckpt_path):
@@ -123,5 +198,5 @@ if __name__ == "__main__":
         
     args = parser.parse_args()
         
-    task = LAMBADA(data_dir=args.data_dir, use_stop_filter=args.stop_word_filter)
+    task = LAMBADA(data_dir=args.data_dir, use_stop_filter=args.stop_word_filter, use_code_data=True)
     task.run(args.model_cfg, args.ckpt_path)
